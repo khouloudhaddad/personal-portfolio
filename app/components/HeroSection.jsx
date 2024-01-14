ee42e54fdadffc56fe36048ca4c775e7e67face0
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-14 12:55:54 +0100

    Hero section

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            khouloud
+          </h1>
+          <p className="text-[#ABB7BE] text-base sm:text-lg lg:text-xl mb-6">
+            Senior Full Stack Developer with over 10 years of experience in
+            developing, and maintaining web applications. Proficient in both
+            front-end and back-end technologies, with a strong track record of
+            delivering high-quality software solutions.
+          </p>
+          <div>
+            <Link
+              href="/"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
