35d74af08f53b41e5ab817bb93a5847d9118e9b8
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-14 14:11:20 +0100

    Mobile menu

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto py-4 px-12">
+      <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
