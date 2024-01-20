f8ff5af83d96b0e59c8b4f767f8e8be1c7591e80
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-20 16:51:32 +0100

    Fix undisplayed color

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="flex items-end text-[#808080] text-xl md:text-4xl text-white font-semibold"
+          className="flex items-end text-light-gray text-xl md:text-4xl text-white font-semibold"
         >
           <Image src="/images/mainLogo.png" width={70} height={70} alt="Logo" />
           ouloud
